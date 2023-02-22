5324a880469eb5643ae5e030bf41ab86e4b551a5
Aman Yasir <amanyasir2013@hotmail.com>
2023-02-22 15:18:30 +0000

    Added styling to the default react app to make it more appealing and fit to purpose

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header-banner'>
+        <h1>Sextant</h1>
+
+        <div className='navbar'>
+
+        <ul>
+          <li>Home</li>
+          <li>Dashboard</li>
+          <li>Settings</li>
+        </ul>
+        
+        </div>
+
+        
+      </div>
+
+      <div className='middle-section'>
+        
+        <div className='box-containers'>
+
+          <div className='box'>
+            <h4>IP Address</h4>
+            <div className='top-border'/>
+            <p>IP: 127.0.0.1</p>
+
+          </div>
+
+          <div className='box'>
+            <h4>Latency Data</h4>
+            <div className='top-border'/>
+            <p>Ping: 0ms</p>
+          </div>
+
+          <div className='box'>
+            <h4>Data Points</h4>
+            <div className='top-border'/>
+            <p>Null</p>
+          </div>
+
+        </div>
+
+      </div>
+      
     </div>
   );
 }
